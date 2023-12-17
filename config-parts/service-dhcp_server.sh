c11acf9681791abf42b861c36a7ae2fdd8e1e0ee
Stephen Brown <sbrown@fastmail.com>
2023-12-17 17:40:50 -0500

    Correct dhcp range nameserver IP

--- config-parts/service-dhcp_server.sh
+++ config-parts/service-dhcp_server.sh
@@ -10,7 +10,7 @@ set service dhcp-server shared-network-name vlan-100 ping-check
 set service dhcp-server shared-network-name vlan-100 subnet 10.0.100.0/24 default-router '10.0.100.254'
 set service dhcp-server shared-network-name vlan-100 subnet 10.0.100.0/24 domain-name 'sblan.network'
 set service dhcp-server shared-network-name vlan-100 subnet 10.0.100.0/24 lease '86400'
-set service dhcp-server shared-network-name vlan-100 subnet 10.0.100.0/24 name-server '10.0.100.254'
+set service dhcp-server shared-network-name vlan-100 subnet 10.0.100.0/24 name-server '10.0.5.4'
 set service dhcp-server shared-network-name vlan-100 subnet 10.0.100.0/24 range 0 start '10.0.100.26'
 set service dhcp-server shared-network-name vlan-100 subnet 10.0.100.0/24 range 0 stop '10.0.100.200'
 
